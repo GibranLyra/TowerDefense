eb5526c8705c8b182f8a6f1b9dc30ff4f0abdcfa
Gibran Lyra <lyra.gibran@gmail.com>
2014-01-16 00:06:02 -0200

    Health Bar Pronto, agora é uma imagem em barra segue o personagem e tem o nome do inimigo em cima da barra

--- Assets/_Scripts/HealthBar.cs
+++ Assets/_Scripts/HealthBar.cs
@@ -1,37 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HealthBar : MonoBehaviour 
+public class HealthBar : MonoBehaviour
 {
-    public Health healthComponent;
-    public Texture2D HpBarTexture;
-    public float healthBarLength;
-    float percentOfHealth;
+    public string playerName;
+    private GUIStyle enemyLabelStyle;
+    private Health healthScript;
+    private float healthBarLength;
+
+    private int healthBarLeft = 100;    
+    private int healthBarTop = 30;
+
+    private int enemyLabelTop;
+    private int enemyLabelLeft = 100;
+
+    private int labelTop;
+    private int labelLeft = 50;
+
+    public Transform AboveOf;    //What is the  object that the bar will be above
+    
 
     void Awake()
     {
-        if (healthComponent)
-            healthComponent.OnSetHealth += SetText;
-        
+        setEnemyGUIStyle();
+
+        healthScript = (Health)transform.parent.gameObject.GetComponent("Health");
+
+        labelTop = healthBarTop + 3;
+        enemyLabelTop = healthBarTop + 56;
     }
-
-
-
-    void SetText(float health)
-    {
-        percentOfHealth = healthComponent.currentHealth / healthComponent.maxHealth;
-        int timesToRepeatSymbol = (int)(percentOfHealth *10);
-        string HealthSymbol = new string('_', timesToRepeatSymbol);
-
-        //GetComponent<TextMesh>().text = "<color=red> Health: " + health.ToString("0") + "/" + healthComponent.maxHealth + "</color>";
-        GetComponent<TextMesh>().text = "<color=red>" + HealthSymbol + "  " + timesToRepeatSymbol  +"</color>";        
-        
-    }
-
     void Update()
     {
+        healthBarLength = (healthScript.currentHealth / healthScript.maxHealth) * 100;
+    }
 
-        transform.LookAt(Camera.main.transform);        
+    void OnGUI()
+    {
+        //worldPosition = new Vector3(myTransform.position.x, myTransform.position.y + adjustment, myTransform.position.z);
+        Vector3 screenPosition =  Camera.main.WorldToScreenPoint(AboveOf.position);
+
+        //creating a ray that will travel forward from the camera's position    
+        var ray = new Ray(Camera.main.transform.position, transform.forward);        
+        var forward = transform.TransformDirection(Vector3.forward);
+        float distance = Vector3.Distance(Camera.main.transform.position, transform.position); //gets the distance between the camera, and the intended target we want to raycast to
+
+        //if something obstructs our raycast, that is if our characters are no longer 'visible,' dont draw their health on the screen.
+        if (!Physics.Raycast(ray, distance))
+        {
+            //HealthBarColor
+            GUI.color = Color.red;
+            GUI.HorizontalScrollbar(new Rect(screenPosition.x - healthBarLeft / 2, Screen.height - screenPosition.y - healthBarTop, 100, 0), 0, healthScript.currentHealth, 0, healthScript.maxHealth); //displays a healthbar
+            //FontColor Health Label
+            GUI.color = Color.white;         
+            GUI.Label(new Rect(screenPosition.x - labelLeft / 2, Screen.height - screenPosition.y - labelTop, 100, 100), healthScript.currentHealth + "/" + healthScript.maxHealth); //displays health in text format
+            //Enemy Name Label
+            GUI.Label(new Rect(screenPosition.x - enemyLabelLeft / 2, Screen.height - screenPosition.y - enemyLabelTop, 100, 100), playerName, enemyLabelStyle); //displays health in text format
+        }
+    }
+
+    void setEnemyGUIStyle()
+    {
+        enemyLabelStyle = new GUIStyle();
+        enemyLabelStyle.normal.textColor = Color.green;
+        enemyLabelStyle.fontSize = 12;
+        enemyLabelStyle.fontStyle = FontStyle.Bold;
+        enemyLabelStyle.clipping = TextClipping.Overflow;
+        enemyLabelStyle.alignment = TextAnchor.MiddleCenter;
     }
 
 }
